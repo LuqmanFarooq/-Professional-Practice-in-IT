a205c229882abc068f68db8a2a09d528be7e665d
LuqmanFarooq <luqifarooq@gmail.com>
2020-02-11 03:57:50 +0000

    initial project setup done all packages installed andimported , created addpage for admin

---
+++ e-commerce-app/models/page.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+// page schema
+
+var pageSchema = mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true
+    },
+    slug: {
+        type: String,
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    sorting: {
+        type: Number,
+    }
+});
+
+var page = module.exports = mongoose.model('page',pageSchema);
