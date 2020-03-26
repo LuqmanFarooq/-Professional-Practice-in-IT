97f95dbc4d92f8cf7921e8ace762caf16c3df07f
Nomijee <g00351754@gmit.ie>
2020-03-26 02:45:51 +0000

    Added paypal functionaloity
    checkout completed
    
    Co-Authored-By: Muhammad Luqman <luqmanfarooq@users.noreply.github.com>

--- routes/cart.js
+++ routes/cart.js
@@ -53,10 +53,67 @@ router.get('/add/:product', function (req, res) {
 
 // Get Checkout Page
 router.get('/checkout', function (req, res) {
-    res.render('checkout', {
-        title: 'Checkout',
-        cart: req.session.cart
-    });
+    if (req.session.cart && req.session.cart.length == 0) {
+        delete req.session.cart;
+        res.redirect('/cart/checkout');
+    } else {
+        res.render('checkout', {
+            title: 'Checkout',
+            cart: req.session.cart
+        });
+    }
+});
+
+
+// Get update product
+router.get('/update/:product', function (req, res) {
+    var slug = req.params.product;
+    var cart = req.session.cart;
+    var action = req.query.action;
+
+    for (var i = 0; i < cart.length; i++) {
+        if (cart[i].title == slug) {
+            switch (action) {
+                case "add":
+                    cart[i].qty++;
+                    break;
+
+                case "remove":
+                    cart[i].qty--;
+                    if (cart[i].qty < 1) {
+                        cart.splice(i, 1);
+                    }
+                    break;
+
+                case "clear":
+                    cart.splice(i, 1);
+                    if (cart.length == 0) {
+                        delete req.session.cart;
+                    }
+                    break;
+                default:
+                    console.log('update not working');
+                    break;
+            }
+            break;
+        }
+    }
+    req.flash('success', 'Cart Updated!');
+    res.redirect('/cart/checkout');
+});
+
+// Get Clear cart
+router.get('/clear', function (req, res) {
+    delete req.session.cart;
+    req.flash('success', 'Cart Cleared!');
+    res.redirect('/cart/checkout');
+});
+
+// Get Buy cart
+router.get('/buy', function (req, res) {
+    delete req.session.cart;
+    
+    res.sendStatus(200);
 });
 
 // Exports
